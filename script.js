7b11fae808cb904c82631dde4126d5cfe91d6018
IMRamAgrawal <123664629+IMRamAgrawal@users.noreply.github.com>
2023-03-12 03:16:49 -0700

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// var menu = document.querySelector(".menu-icon")
-// var left = document.querySelector(".left-content")
-// menu.onclick = function(){
-//  left.classList.add("short")
-// }
-// let a = "tdfykgujlk"
-// console.log(a)
